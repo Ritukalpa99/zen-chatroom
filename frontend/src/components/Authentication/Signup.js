8d9b14eb078c9c17af73669b4fd775b0d0ff21b2
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2024-02-09 02:02:55 +0530

    cloudinary image, signup component frontend connected to backend

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -6,8 +6,11 @@ import {
 	VStack,
 	InputRightElement,
 	Button,
+	useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import axios from 'axios'
+import { useNavigate} from 'react-router-dom'
 
 const Signup = () => {
 	const [show, setShow] = useState(false);
@@ -15,17 +18,115 @@ const Signup = () => {
 	const [email, setEmail] = useState();
 	const [password, setPassword] = useState();
 	const [confirmPassword, setConfirmPassword] = useState();
+	const [loading, setLoading] = useState(false);
 	const [pic, setPic] = useState();
+
+	const toast = useToast();
+
+	const navigate = useNavigate();
 
 	const handleClick = () => setShow(!show);
 
-    const postDetails = (pics) => {
+	const postDetails = (pics) => {
+		setLoading(true);
+		if (pics === undefined) {
+			toast({
+				title: "Please Select an Image",
+				status: "warning",
+				duration: 5000,
+				isClosable: true,
+				position: "bottom",
+			});
+			return;
+		}
 
-    }
+		if (pics.type === "image/jpeg" || pics.type === "image/png") {
+			const data = new FormData();
+			data.append("file", pics);
+			data.append("upload_preset", "zen-chatroom");
+			data.append("cloud_name", "jinn-space");
 
-    const submitHandler = () => {
-        
-    }
+			fetch("https://api.cloudinary.com/v1_1/jinn-space/image/upload", {
+				method: "POST",
+				body: data,
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					setPic(data.url.toString());
+					console.log(data.url.toString());
+					setLoading(false);
+				})
+				.catch((err) => {
+					console.log(err);
+					setLoading(false);
+				});
+		} else {
+			toast({
+				title: "Please Select an Image",
+				status: "warning",
+				duration: 5000,
+				isClosable: true,
+				position: "bottom",
+			});
+			setLoading(false);
+			return;
+		}
+	};
+
+	const submitHandler =async () => {
+		setLoading(true);
+		if(!name || !email || !password || !confirmPassword) {
+			toast({
+				title: "Please fill all the details",
+				status : "warning",
+				duration : 5000,
+				isClosable : true,
+				position : "bottom"
+			});
+			setLoading(false);
+			return;
+		}
+		if(password !== confirmPassword) {
+			toast({
+				title: "Passwords Do Not Match",
+				status : "warning",
+				duration : 5000,
+				isClosable : true,
+				position : "bottom"
+			});
+			setLoading(false);
+			return;
+		}
+		try {
+			const config = {
+				headers : {
+					"Content-Type" : "application/json",
+				}
+			};
+
+			const {data} = await axios.post('/api/user',{name,email,password,pic}, config);
+			toast({
+				title: "Registration Successful",
+				status : "success",
+				duration : 5000,
+				isClosable : true,
+				position : "bottom"
+			});
+			localStorage.setItem('userInfo', JSON.stringify(data));
+			setLoading(false);
+			navigate('/chats')
+		}catch(error) {
+			toast({
+				title: "Error Occured!",
+				description : error.response.data.message,
+				status : "success",
+				duration : 5000,
+				isClosable : true,
+				position : "bottom"
+			});
+		}
+
+	};
 
 	return (
 		<VStack spacing="5px">
@@ -73,15 +174,25 @@ const Signup = () => {
 					</InputRightElement>
 				</InputGroup>
 			</FormControl>
-            <FormControl>
-                <FormLabel>Upload your Picture</FormLabel>
-                <Input type="file"p={1.5} accept="image/" onChange={(e) => postDetails(e.target.files[0])} />
-            </FormControl>
+			<FormControl>
+				<FormLabel>Upload your Picture</FormLabel>
+				<Input
+					type="file"
+					p={1.5}
+					accept="image/"
+					onChange={(e) => postDetails(e.target.files[0])}
+				/>
+			</FormControl>
 
-            <Button colorScheme="blue" width="100%" style={{marginTop : 15}}
-            onClick={submitHandler}>
-                Sign Up
-            </Button>
+			<Button
+				colorScheme="blue"
+				width="100%"
+				style={{ marginTop: 15 }}
+				onClick={submitHandler}
+				isLoading={loading}
+			>
+				Sign Up
+			</Button>
 		</VStack>
 	);
 };
