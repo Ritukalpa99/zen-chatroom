40c6abeece3ddb2093a3e490887d1a83df344d46
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2024-02-13 13:16:49 +0530

    accessChat and fetchChat api done

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const  chats = require('./data/data');
 const connectDB = require('./config/db');
-const colors = require('colors')
-const userRoutes = require('./routes/userRoutes')
+const colors = require('colors');
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 const {notFound, errorHandler} = require('./middleware/errorMiddleware');
 
 dotenv.config();
@@ -17,7 +18,8 @@ app.get('/', (req,res) => {
     res.send("API is Running")
 })
 
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
